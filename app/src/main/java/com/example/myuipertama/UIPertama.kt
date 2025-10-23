0ae1ca9afd16617220c26b086be03fcd2f3b04e2
Asti22 <uun3461@gmail.com>
2025-10-23 19:45:01 +0700

    membuat colum, menambahkan teks nama

--- app/src/main/java/com/example/myuipertama/UIPertama.kt
+++ app/src/main/java/com/example/myuipertama/UIPertama.kt
@@ -116,7 +116,6 @@ fun ActivitasPertama(modifier: Modifier){
                         color = Color.Black,
                         modifier = Modifier.padding(top = 10.dp)
                     )
-                    Spacer(modifier = Modifier.width(width = 30.dp))
                 }
             }
         }
@@ -133,6 +132,16 @@ fun ActivitasPertama(modifier: Modifier){
                     contentDescription = null,
                     modifier = Modifier.size(size = 100.dp).padding(all = 5.dp)
                 )
+                Spacer(modifier = Modifier.width(width = 30.dp))
+                Column (){
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.Black,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+
 
         }
 
